9a5979d40a8676e50891e3263ce7dd1b9d6fcd14
Surendar <s.surendar11@gmail.com>
2025-01-07 12:30:08 +0530

    Create trivy.sh

---
+++ trivy.sh
@@ -0,0 +1,10 @@
+#Install Trivy in Jenkins server
+#https://trivy.dev/v0.18.3/installation/
+#Debian/Ubuntu
+#Add repository to /etc/apt/sources.list.d.
+
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
